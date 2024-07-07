4006e9fd386450d177f4cf923110e44f23058c27
Bhavani.P <bhavanic7@gmail.com>
2024-07-07 13:35:06 +0530

    hey tes

---
+++ src/July7th/LAB001.java
@@ -0,0 +1,11 @@
+package July7th;
+
+public class LAB001 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+
+
---
+++ src/July7th/LAB002.java
@@ -0,0 +1,8 @@
+package July7th;
+
+public class LAB002 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+}
