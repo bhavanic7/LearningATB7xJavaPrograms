9327768f19a09a64e424a32533d5c7e7aaa53cdd
Bhavani.P <bhavanic7@gmail.com>
2024-07-07 14:43:38 +0530

    new lab

--- src/July7th/LAB004.java
+++ src/July7th/LAB004.java
@@ -2,6 +2,6 @@ package July7th;
 
 public class LAB004 {
     public static void main(String[] args) {
-        System.out.println("git was successful");
+        System.out.println("git was successful again");
     }
 }
