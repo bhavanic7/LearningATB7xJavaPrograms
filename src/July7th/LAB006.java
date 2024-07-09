de9d07550582b36f5955ad9413ac3db41e655e15
Bhavani.P <bhavanic7@gmail.com>
2024-07-09 11:20:41 +0530

    new lab

--- src/July7th/LAB006.java
+++ src/July7th/LAB006.java
@@ -1,14 +1,8 @@
 package July7th;
 
 public class LAB006 {
-    public void testValues() {
-        int a = 10;
-        int b = 25;
-    }
-
     public static void main(String[] args) {
-
-        LAB006 d = new LAB006();
-        d.testValues();
+        System.out.println("test commit");
     }
-}
+
+    }
