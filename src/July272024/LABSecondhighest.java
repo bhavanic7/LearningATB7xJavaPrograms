a27b3b44253d975d147e27f8726fd0e99f7a57a8
Bhavani.P <bhavanic7@gmail.com>
2024-08-06 21:12:28 +0530

    Latest

--- src/July272024/LABSecondhighest.java
+++ src/July272024/LABSecondhighest.java
@@ -2,15 +2,29 @@ package July272024;
 
 public class LABSecondhighest {
     public static void main(String[] args) {
-        // Find the Second highest salary in the Arrays
+        // Find the Secondhighest salary in the Arrays
+        // Logic is first sort numbers in ascending order
+        // then print the second number from array which is array index no 1
+
         int[] salaries = {7, 10, 4, 13, 25, 18, 9, 8, 17};
-        int temp;
-        for (int i = 0; i < salaries.length; i++) {
 
 
-            System.out.println("sechondhighest" + SecondMax);
+        for (int i = 0; i < salaries.length; i++) { // this loop is for salaries array
+            for(int j=i+1; j<salaries.length; j++){ // this loop starts from 1 of salaries array
+                if (salaries [i] < salaries[j]){ // this condition will start comparing numbers
+                  int temp = salaries[i]; // if i value is less than j its value will be stored in temp
+                    salaries [i] = salaries [j]; //
+                    salaries[j] = temp;
+
+
+                }
+
+            }
+
         }
+        System.out.println("sechondhighest " + salaries[1]);
     }
-}
+    }
+
 
 
